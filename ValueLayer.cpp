d37d0751b7f63238f0563fb6aab0f24a6cd181e1
Tosiekdev <antonizieciak@gmail.com>
2024-10-19 21:29:23 +0200

    Neighborhood now returns vector of points.

--- ValueLayer.cpp
+++ ValueLayer.cpp
@@ -1,35 +1,43 @@
 #include "ValueLayer.hpp"
 
+#include <unistd.h>
+
 namespace abmf {
 template<typename T>
-T& ValueLayer<T>::get(Point pos) {
-    return grid[pos.y][pos.x];
+T ValueLayer<T>::get(Point pos) {
+    return read[pos.y][pos.x];
 }
 
 template<typename T>
-Moore<T> ValueLayer<T>::getMoore(Point pos) {
+std::vector<Point> ValueLayer<T>::getMoore(Point pos, size_t radius) {
     auto [x, y] = pos;
-    return Moore<T>(
-        get({x - 1, y + 1}),
-        get({x, y + 1}),
-        get({x + 1, y + 1}),
-        get({x + 1, y}),
-        get({x + 1, y - 1}),
-        get({x, y - 1}),
-        get({x - 1, y - 1}),
-        get({x - 1, y}),
-        get(pos));
+    return {
+        {x - 1, y + 1},
+        {x, y + 1},
+        {x + 1, y + 1},
+        {x + 1, y},
+        {x + 1, y - 1},
+        {x, y - 1},
+        {x - 1, y - 1},
+        {x - 1, y},
+        pos};
 }
 
 template<typename T>
-VonNeumann<T> ValueLayer<T>::getVonNeumann(Point pos) {
+std::vector<Point> ValueLayer<T>::getVonNeumann(Point pos, size_t radius) {
     auto [x, y] = pos;
-    return VonNeumann<T>(
-        get({x, y + 1}),
-        get({x + 1, y}),
-        get({x, y - 1}),
-        get({x - 1, y}),
-        get(pos));
+    return {
+        {x, y + 1},
+        {x + 1, y},
+        {x, y - 1},
+        {x - 1, y},
+        {pos}
+};
+}
+
+template<typename T>
+void ValueLayer<T>::swap() {
+    read.swap(write);
 }
 
 template class ValueLayer<int>;
--- ValueLayer.hpp
+++ ValueLayer.hpp
@@ -9,14 +9,15 @@ namespace abmf {
 template<typename T>
 class ValueLayer {
 public:
-    explicit ValueLayer(size_t width, size_t height) : grid(height, std::vector<T>(width)) {}
-    T& get(Point pos);
-    Moore<T> getMoore(Point pos);
-    VonNeumann<T> getVonNeumann(Point pos);
-    T sumMoore(bool center);
+    explicit ValueLayer(size_t width, size_t height) : read(height, std::vector<T>(width)), write(read) {}
+    T get(Point pos);
+    std::vector<Point> getMoore(Point pos, size_t radius=1);
+    std::vector<Point> getVonNeumann(Point pos, size_t radius=1);
+    void swap();
 
 private:
-    std::vector<std::vector<T>> grid;
+    std::vector<std::vector<T>> read;
+    std::vector<std::vector<T>> write;
 };
 
 using IntValueLayer = ValueLayer<int>;
