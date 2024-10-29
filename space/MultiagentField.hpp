57fa6790b1f953608be06f31fd29b37386031186
Tosiekdev <antonizieciak@gmail.com>
2024-10-29 07:02:59 +0100

    Declared first attributes of MultiagentField

---
+++ space/MultiagentField.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Point.hpp"
+
+#include <concepts>
+#include <optional>
+#include <variant>
+#include <vector>
+
+namespace abmf {
+template<typename A>
+concept Positionable = requires(A a) {
+    { a.pos } -> std::same_as<std::optional<Point>>;
+};
+
+template<Positionable... Agents>
+class MultiagentField {
+public:
+    using AgentT = std::variant<std::reference_wrapper<Agents>...>;
+    using SquareT = std::vector<AgentT>;
+    using GridT = std::vector<std::vector<SquareT>>;
+
+private:
+    GridT grid;
+};
+}
