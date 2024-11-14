6cc42ce5ccd2bba16d97794a85ce7b887028d879
Tosiekdev <antonizieciak@gmail.com>
2024-11-14 19:13:03 +0100

    Fix in moving agent.

--- space/FieldImpl.hpp
+++ space/FieldImpl.hpp
@@ -17,8 +17,11 @@ template<Positionable ... Agents>
 template<Positionable Agent> requires (std::is_same_v<Agent, Agents> || ...)
 bool Field<Agents...>::moveAgent(Agent& agent, Point pos) {
     if (grid[pos.y][pos.x]) return false;
-    if (grid[agent.pos.y][agent.pos.x]) {
-        grid[agent.pos.y][agent.pos.x] = std::nullopt;
+    if (!agent.pos) {
+        return addAgent(agent, pos);
+    }
+    if (grid[agent.pos.value().y][agent.pos.value().x]) {
+        grid[agent.pos.value().y][agent.pos.value().x] = std::nullopt;
     }
     grid[pos.y][pos.y] = &agent;
     agent.pos = pos;
