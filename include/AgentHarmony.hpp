2437d30c44e33d4c9949a4550204092c0bc40856
Tosiekdev <antonizieciak@gmail.com>
2024-12-14 21:38:49 +0100

    Aggregated includes files.

---
+++ include/AgentHarmony.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "model/Model.hpp"
+#include "schedule/Schedule.hpp"
+#include "space/Space.hpp"
+#include "utilities/Concepts.hpp"
+#include "utilities/Utils.hpp"
---
+++ include/space/Space.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "Edge.hpp"
+#include "Field.hpp"
+#include "MultiagentField.hpp"
+#include "Network.hpp"
+#include "ValueLayer.hpp"
